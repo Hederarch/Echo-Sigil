58144d35e7ac3a4937c607eb90bc6143c8c23a39
Portal <57550537+Theportalmasters@users.noreply.github.com>
2020-06-23 12:28:07 -0500

    Main Menu
    Made a nice little main menu

---
+++ Echo-Sigil/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadGame()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void MapEditor()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
